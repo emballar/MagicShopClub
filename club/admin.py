1d6b7fcb33dbae6d17a189614704b3be86e06b76
emballar <erinmichelle.ballar@gmail.com>
2022-06-05 22:42:42 -0700

    models commit

--- club/admin.py
+++ club/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import TourInfo, Meetup
 # Register your models here.
+admin.site.register(TourInfo)
+admin.site.register(Meetup)
--- club/models.py
+++ club/models.py
@@ -1,3 +1,32 @@
+
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class TourInfo(models.Model):
+    title=models.CharField(max_length=255)
+    location=models.CharField(max_length=255)
+    date=models.DateField()
+    time=models.CharField(max_length=255)
+    price=models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table='tourinfo'
+
+class Meetup(models.Model):
+    title=models.CharField(max_length=255)
+    date=models.DateField()
+    location=models.CharField(max_length=255)
+    time=models.CharField(max_length=255)
+    description=models.TextField(null=True, blank=True)
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table='meetup'
