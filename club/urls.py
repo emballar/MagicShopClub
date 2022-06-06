300f19a75ad4342156ecbdecb97d486de72dd0ea
emballar <erinmichelle.ballar@gmail.com>
2022-06-05 23:14:24 -0700

    views commit 1

--- club/urls.py
+++ club/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('tourinfo/', views.tourinfo, name='tourinfo'),
+    path('meetup/', views.meetup, name='meetup')
 ]
--- club/views.py
+++ club/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import TourInfo, Meetup
 
 # Create your views here.
 def index(request):
     return render(request, 'club/index.html')
+
+def tourinfo(request):
+    tour_list=TourInfo.objects.all()
+    return render(request, 'club/tourinfo.html', {'tour_list' : tour_list})
+
+def meetup(request):
+    meetup_list=Meetup.objects.all()
+    return render(request, 'club/meetup.html', {'meetup_list' : meetup_list})
